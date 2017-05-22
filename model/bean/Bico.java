7ed1bc4cbccb3feab6075cba0bf73356671ae2d7
Igor Remas Cordeiro <igoremascordeiro@hotmail.com>
2017-05-22 19:39:03 -0300

    Modificando variaveis para properties

--- model/bean/Bico.java
+++ model/bean/Bico.java
@@ -2,17 +2,24 @@ package model.bean;
 
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 public class Bico extends RecursiveTreeObject<Bico> {
-	private Integer id;
+	private IntegerProperty id = new SimpleIntegerProperty();
 	private Bomba bomba;
 	private TanqueReparticao reparticao;
 	
-	public Integer getId() {
-		return id;
+	public final Integer getId() {
+		return id.get();
 	}
 	
-	public void setId(Integer id) {
-		this.id = id;
+	public final void setId(Integer Id) {
+		id.set(Id);
+	}
+	
+	public IntegerProperty idProperty() {
+		return id;
 	}
 	
 	public Bomba getBomba() {
